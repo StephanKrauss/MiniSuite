70d58145643e1ee76bfb8b3e915af140ca8f9c56
Aurélien Delogu <dev@dreamysource.fr>
2017-08-18 17:43:06 +0200

    Add base PHP assert code

--- src/Suite.php
+++ src/Suite.php
@@ -8,6 +8,30 @@ namespace MiniSuite;
 class Suite implements SuiteInterface
 {
     /**
+     * Suite name
+     *
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * Test list
+     *
+     * @var array
+     */
+    protected $tests = [];
+
+    /**
+     * Constructor
+     *
+     * @param string $name
+     */
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    /**
      * Add a test
      *
      * @param string $name
@@ -16,7 +40,7 @@ class Suite implements SuiteInterface
      */
     public function add($name, callable $test) : void
     {
-
+        $this->tests[$name] = $test;
     }
 
     /**
@@ -26,6 +50,19 @@ class Suite implements SuiteInterface
      */
     public function run() : void
     {
-
+        // Prepare
+        $helper = new Helper();
+        // Change directives
+        $assertException = ini_get('assert.exception');
+        ini_set('assert.exception', '1');
+        // Run tests
+        foreach ($this->tests as $name => $test) {
+            try {
+                call_user_func($test, $helper);
+            }
+            catch() {}
+        }
+        // Restore directives
+        ini_set('assert.exception', $assertException);
     }
 }
