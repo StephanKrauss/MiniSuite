79dc1254ec1d57b2973a3e0017f07aa8d6442a50
Aurélien Delogu <dev@dreamysource.fr>
2017-09-09 11:25:26 +0200

    Clean up Suite interface

--- src/SuiteInterface.php
+++ src/SuiteInterface.php
@@ -8,22 +8,6 @@ namespace MiniSuite;
 interface SuiteInterface
 {
     /**
-     * Constructor
-     *
-     * @param string $name
-     */
-    public function __construct(string $name);
-
-    /**
-     * Add a test
-     *
-     * @param string $name
-     * @param callable $test
-     * @return void
-     */
-    public function add($name, callable $test) : void;
-
-    /**
      * Run the tests
      *
      * @return array
