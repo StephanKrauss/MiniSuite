aaab46c16d06748f276f0f5ce1e76af5a79396fc
Aurélien Delogu <dev@dreamysource.fr>
2017-09-10 20:43:26 +0200

    Add example

--- tests/tests.php
+++ tests/tests.php
@@ -9,10 +9,14 @@ error_reporting(E_ALL);
 require 'vendor/autoload.php';
 require '../vendor/autoload.php';
 
-$loader = new Psr4ClassLoader;
+$loader = new Psr4ClassLoader();
 $loader->addPrefix('MiniSuite\\', '../src');
 $loader->register();
 
 ########################################################### Base
 
 $minisuite = new MiniSuite\Suite('Base');
+
+$minisuite->add('test #1', function ($assert) {
+    $assert(72)->isNotNull()->equals(72);
+});
